69670517f1b4d346fcc50ec07ccf1199538b8226
lev._.hyun <110030247+levhyun@users.noreply.github.com>
2023-01-25 22:42:32 +0900

    .

---
+++ public/js/main.js
@@ -0,0 +1,48 @@
+const socket = io();
+const btnSend = document.getElementById("send-message");
+const message = document.getElementById("message-area");
+const boxMessages = document.getElementById("chat-box");
+
+btnSend.addEventListener("click", () => {
+  if (message.value == "") {
+    message.focus();
+  } 
+  else {
+    boxMessages.innerHTML += `
+      <!-- MI MENSAJE -->
+  <div class="chat from-message">
+    <div class="detalles">
+      <p>${message.value}</p>
+    </div>
+  </div>
+      `;
+    scrollBottom();
+    socket.emit("message", { msg: message.value });
+    message.value = null;
+  }
+});
+
+function enterkey() {
+  keyenter = event.keyCode;
+  if (keyenter == 13) {
+    btnSend.click();
+    scrollBottom();
+  }
+}
+window.onkeydown = enterkey;
+
+function scrollBottom() {
+  boxMessages.scrollTop = boxMessages.scrollHeight;
+}
+
+socket.on("message", (data) => {
+  boxMessages.innerHTML += `
+  <!-- MENSAJE AMIGO -->
+  <div class="chat to-message">
+    <div class="detalles">
+      <p>${data.msg}</p>
+    </div>
+  </div>
+  `;
+  scrollBottom()
+});
