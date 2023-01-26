21febea1c81a7962f2ed892e68418989c985452e
lev._.hyun <110030247+levhyun@users.noreply.github.com>
2023-01-26 11:29:08 +0900

    .

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const SocketIO = require('socket.io')
+var os = require('os');
 
 app.use((express.static(path.join(__dirname, "public"))))
 
@@ -9,7 +10,7 @@ app.use((express.static(path.join(__dirname, "public"))))
 app.set("port", process.env.PORT || 3000);
 
 const server = app.listen(app.get("port"), () =>{
-    console.log("server port:", app.get("port"));
+    console.log("server address:", "http://" + getIp() + ":" + app.get("port"));
 })
 
 const io = SocketIO(server);
@@ -24,3 +25,18 @@ io.on('connection', (socket)=>{
         socket.broadcast.emit("message", data)
     })
 })
+
+function getIp() {
+    var ifaces = os.networkInterfaces();
+    var ip = '';
+    for (var dev in ifaces) {
+        var alias = 0;
+        ifaces[dev].forEach(function(details) {
+            if (details.family == 'IPv4' && details.internal === false) {
+                ip = details.address;
+                ++alias;
+            }
+        });
+    }
+    return ip;
+}
